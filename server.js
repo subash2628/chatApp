edcbcf1941e1dd4c8494d3c9f6e782ce5c204d89
subash <subashsharma2628@example.com>
2022-01-12 07:59:07 +0545

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const io = require("socket.io")(3000,{
+    cors:{
+        origin:["http://localhost:8080"],
+    },
+})
+
+
+io.on("connection", socket => {
+    //console.log(socket.id)
+    socket.on("send-message", message=>{
+        socket.broadcast.emit("receive-message",message)
+    })
+})
+
+
